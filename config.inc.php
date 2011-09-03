43f452b57fd83d5de86e56f36c8261592033dbfa
Yevgeniy A. Viktorov <yeevgen@gmail.com>
2011-09-03 14:44:51 +0300

    change to loopback IP

--- config.inc.php
+++ config.inc.php
@@ -28,9 +28,9 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = '127.0.0.1';
 $cfg['Servers'][$i]['connect_type'] = 'socket';
-$cfg['Servers'][$i]['socket']       = 'localhost;unix_socket=/tmp/mysql/willene.sock';
+$cfg['Servers'][$i]['socket']       = '/tmp/mysql/willene.sock';
 $cfg['Servers'][$i]['compress'] = false;
 /* Select mysqli if your server has it */
 $cfg['Servers'][$i]['extension'] = 'mysql';
